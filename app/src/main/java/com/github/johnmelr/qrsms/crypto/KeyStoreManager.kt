06bfe7873a7cdc82d45f79fbb988f6cb11bfa37c
johnmelr <jrramos7@up.edu.ph>
2024-06-09 17:44:18 +0800

    Added abstraction for key management
    Instead of having to initialize different class for different API
    levels, having an interface and abstract class that the key manager
    class can extend makes it easier to use the KeyManager class without
    having to modify how it is called in the classes that uses it.
    
    Without the interface and abstract class, we would have to instantiate
    different key manager class depending on the API level in each of the
    class that needs to use the functions of a key manager.

--- app/src/main/java/com/github/johnmelr/qrsms/crypto/KeyStoreManager.kt
+++ app/src/main/java/com/github/johnmelr/qrsms/crypto/KeyStoreManager.kt
@@ -1,36 +1,26 @@
 package com.github.johnmelr.qrsms.crypto
 
-import android.security.keystore.KeyProperties
-import android.security.keystore.KeyProtection
 import android.telephony.PhoneNumberUtils
 import android.util.Log
 import java.security.Key
 import java.security.KeyStore
-import java.security.KeyStore.SecretKeyEntry
-import java.security.MessageDigest
 import java.security.PrivateKey
 import java.security.PublicKey
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import javax.crypto.spec.SecretKeySpec
 
-const val KEYSTORE_MANAGER_TAG = "KeyStoreManager"
-const val NO_ENTRY_FOR_ALIAS = "No entry with the given alias exist in the Android Key Store"
+private const val KEYSTORE_MANAGER_TAG = "KeyStoreManager"
+private const val NO_ENTRY_FOR_ALIAS = "No entry with the given alias exist in the Android Key Store"
+private const val EC_KEY_PREFIX = "eckey_"
 
-object KeyStoreManager {
-    private const val ANDROID_KEY_STORE = "AndroidKeyStore"
-    private const val EC_KEY_PREFIX = "eckey_"
-    private const val SECRET_KEY_PREFIX = "skey_"
-
-    private val keyStore = KeyStore.getInstance(ANDROID_KEY_STORE).apply {
-        load(null)
-    }
+class KeyStoreManager(
+   private val keyStore: KeyStore = KeyStore
+       .getInstance("AndroidKeyStore").apply { load(null) }
+): KeyManager(keyStore) {
 
     /**
      * Retrieves Public Key associated with the given phone number. This association
      * is done when user generates a QR Code after selecting a contact.
      */
-    fun getPublicKeyForNumber(phoneNumber: String): PublicKey? {
+    override fun getPublicKeyForNumber(phoneNumber: String): PublicKey? {
         val alias = EC_KEY_PREFIX +
                 PhoneNumberUtils.formatNumberToE164(phoneNumber, "PH")
 
@@ -49,7 +39,7 @@ object KeyStoreManager {
      * is done when user generates a QR Code after selecting a contact. This private key
      * will be used to perform an ECDH key agreement.
      */
-    fun getPrivateKeyForNumber(phoneNumber: String): PrivateKey? {
+    override fun getPrivateKeyForNumber(phoneNumber: String): PrivateKey? {
         val alias = EC_KEY_PREFIX +
                 PhoneNumberUtils.formatNumberToE164(phoneNumber, "PH")
 
@@ -63,51 +53,8 @@ object KeyStoreManager {
         return key as PrivateKey?
     }
 
-    /**
-     * Retrieves Secret Key entry to be used for encrypting a message for a contact.
-     *
-     * @param phoneNumber Contact's phone number used as a the alias for the KeyStore entry
-     * @return the associated SecretKey for the given contact
-     */
-    fun getSecretKey(phoneNumber: String): SecretKey? {
-        val alias = SECRET_KEY_PREFIX +
-                PhoneNumberUtils.formatNumberToE164(phoneNumber, "PH")
-
-        val key = keyStore.getKey(alias, null)
-
-        return if (key != null) key as SecretKey else null
-    }
-
-    /**
-     * Stores the generated secret key in keystore using the phone number as the alias.
-     */
-    fun saveSecretKey(phoneNumber: String, secretKeyByte: ByteArray) {
-        // Create SecretKey instance from ByteArray
-        val secretKeySpec = SecretKeySpec(secretKeyByte, KeyProperties.KEY_ALGORITHM_AES)
-        val secretKeyEntry = SecretKeyEntry(secretKeySpec)
-
-        val alias = SECRET_KEY_PREFIX +
-                PhoneNumberUtils.formatNumberToE164(phoneNumber, "PH")
-        keyStore.setEntry(
-            alias,
-            secretKeyEntry,
-            KeyProtection.Builder(
-                KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
-                .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-                .build()
-        )
-    }
-
-    fun doesKeyPairExist(phoneNumber: String): Boolean {
+    override fun doesKeyPairExist(phoneNumber: String): Boolean {
         val alias = EC_KEY_PREFIX + phoneNumber
-
-        return keyStore.containsAlias(alias)
-    }
-
-    fun doesSecretKeyExist(phoneNumber:String): Boolean {
-        val alias = SECRET_KEY_PREFIX +
-                PhoneNumberUtils.formatNumberToE164(phoneNumber, "PH")
 
         return keyStore.containsAlias(alias)
     }
--- app/src/main/java/com/github/johnmelr/qrsms/data/preferencesDataStore/PreferencesRepository.kt
+++ app/src/main/java/com/github/johnmelr/qrsms/data/preferencesDataStore/PreferencesRepository.kt
@@ -4,9 +4,9 @@ import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.byteArrayPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.preferencesOf
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -32,10 +32,46 @@ class PreferencesRepository(private val dataStore: DataStore<Preferences>) {
             defaultPhoneNumber
         }
 
+    val databasePassphrase: Flow<ByteArray> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                Log.e(TAG,"Error reading preferences", exception)
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            val databasePassphrase = preferences[DATABASE_PASSPHRASE] ?: byteArrayOf(0.toByte())
+            databasePassphrase
+        }
+
+    val isFirstLaunch: Flow<Boolean> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                Log.e(TAG,"Error reading preferences", exception)
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            val databasePassphrase = preferences[IS_FIRST_LAUNCH] ?: false
+            databasePassphrase
+        }
+
     suspend fun setDefaultPhoneNumber(newValue: String) {
-        Log.v(TAG, "Setting Default Phone Number: $newValue")
         dataStore.edit { preferences ->
-            preferences[DEFAULT_PHONE_NUMBER] = newValue
+            preferences[DEFAULT_PHONE_NUMBER] = newValue }
+    }
+
+    suspend fun completeFirstLaunch() {
+        dataStore.edit { preferences ->
+            preferences[IS_FIRST_LAUNCH] = false
+        }
+    }
+
+    suspend fun updatePassphrase(passphrase: ByteArray) {
+        dataStore.edit { preferences ->
+            preferences[DATABASE_PASSPHRASE] = passphrase
         }
     }
 
@@ -48,5 +84,7 @@ class PreferencesRepository(private val dataStore: DataStore<Preferences>) {
 
     companion object PreferencesKey {
         val DEFAULT_PHONE_NUMBER = stringPreferencesKey("default_phone_number")
+        val DATABASE_PASSPHRASE = byteArrayPreferencesKey("database_passphrase")
+        val IS_FIRST_LAUNCH = booleanPreferencesKey("is_first_launch")
     }
 }
