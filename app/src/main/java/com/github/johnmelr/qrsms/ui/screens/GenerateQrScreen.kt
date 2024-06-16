7313f44bd7d42fecb2a9f63dc6ba90bc4f95ebba
johnmelr <jrramos7@up.edu.ph>
2024-06-16 15:42:49 +0800

    Modifications in QR Code Generation Screen
    - With the addition of the key Management screen, there is a need to
      enable the action of viewing the QR Code from this screen. To make
      this screen easier to work with, some reusable code have been moved to
    external files.
    - Additional confirmation dialog has also been added to prevent user
      from accidentally replacing the current QR code associated with the
      current contact.

--- app/src/main/java/com/github/johnmelr/qrsms/ui/screens/GenerateQrScreen.kt
+++ app/src/main/java/com/github/johnmelr/qrsms/ui/screens/GenerateQrScreen.kt
@@ -1,42 +1,31 @@
 package com.github.johnmelr.qrsms.ui.screens
 
 import android.Manifest
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.draw
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,13 +33,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.github.johnmelr.qrsms.R
 import com.github.johnmelr.qrsms.data.contacts.ContactDetails
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.EncodeHintType
-import com.google.zxing.qrcode.QRCodeWriter
+import com.github.johnmelr.qrsms.ui.components.QrImage
+import com.github.johnmelr.qrsms.utils.QrUtils.generateQrCode
 
 @Composable
 fun GenerateQrScreen(
@@ -95,28 +81,9 @@ fun GenerateQrScreen(
         )
         val picture = remember { android.graphics.Picture() }
         QrImage(
-            modifier = Modifier
-                .drawWithCache {
-                    val width = this.size.width.toInt()
-                    val height = this.size.height.toInt()
-
-                    onDrawWithContent {
-                        val pictureCanvas =
-                            androidx.compose.ui.graphics.Canvas(
-                                picture.beginRecording(width, height)
-                            )
-                        draw(this, this.layoutDirection, pictureCanvas, this.size) {
-                            this@onDrawWithContent.drawContent()
-                        }
-                        picture.endRecording()
-
-                        drawIntoCanvas { canvas ->
-                            canvas.nativeCanvas.drawPicture(picture)
-                        }
-                    }
-                },
             defaultPhoneNumber = defaultPhoneNumber,
-            imageBitmap = imageBitmap
+            imageBitmap = imageBitmap,
+            picture = picture,
         )
         Row(
             horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -144,6 +111,8 @@ fun GenerateQrScreen(
             }
         }
 
+        val openDialog = remember { mutableStateOf(false) }
+
         if (hasExistingKey) {
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -153,82 +122,31 @@ fun GenerateQrScreen(
                         ?: selectedContact.normalizedPhoneNumber}"
                 )
                 ElevatedButton(
-                    onClick = { generateKeyPair(selectedContact) }) {
+                    onClick = { openDialog.value = true }) {
                     Text(text = "Generate new key")
                 }
             }
         }
-    }
-}
 
-
-
-@Composable
-fun QrImage(
-    modifier: Modifier = Modifier,
-    defaultPhoneNumber: String,
-    imageBitmap: ImageBitmap?,
-) {
-    if (imageBitmap == null) return
-
-    val shape = RoundedCornerShape(16.dp)
-    val color = MaterialTheme.colorScheme.secondaryContainer
-
-    Column(
-        modifier = modifier
-            .background(color, shape)
-            .padding(
-                start = 48.dp,
-                end = 48.dp,
-                top = 16.dp,
-                bottom = 32.dp
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            Text(
-                text = defaultPhoneNumber,
-                fontWeight = FontWeight.Bold,
+        if (openDialog.value) {
+            AlertDialog(
+                onDismissRequest = { openDialog.value = false },
+                confirmButton = { TextButton(onClick = {
+                    generateKeyPair(selectedContact)
+                    openDialog.value = false
+                }) { Text("Confirm") }},
+                dismissButton = { TextButton(onClick = {
+                    openDialog.value = false
+                }) { Text("Cancel") }},
+                title = { Text("Generate QR Code") },
+                text = { Text("Are you sure you want replace your QR Code for ${selectedContact.displayName ?: selectedContact.normalizedPhoneNumber}?") }
             )
-            Text(
-                text = "QRSMS Code",
-                fontWeight = FontWeight.Light,
-                fontSize = 10.sp
-            )
+
         }
-        Image(
-            modifier = Modifier
-                .size(200.dp)
-                .clip(
-                    RoundedCornerShape(12.dp)
-                ),
-            bitmap = imageBitmap,
-            contentDescription = "QR Code",
-        )
+
     }
 }
 
-fun generateQrCode(textToEncode: String): Bitmap {
-    val size = 512 //pixels
-    val hints = hashMapOf<EncodeHintType, Int>().also {
-        it[EncodeHintType.MARGIN] = 2
-    } // Make the QR code buffer border narrower
-    val bits = QRCodeWriter().encode(textToEncode, BarcodeFormat.QR_CODE, size, size, hints)
-
-    return Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
-        for (x in 0 until size) {
-            for (y in 0 until size) {
-                it.setPixel(x, y, if (bits[x, y]) Color.BLACK else Color.WHITE)
-            }
-        }
-    }
-}
 
 @Preview()
 @Composable
